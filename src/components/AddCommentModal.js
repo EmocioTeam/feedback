c23d28e84e48b4d65b7e532a30846f20f2072211
turutupa <adc.batros@gmail.com>
2019-07-05 15:48:18 +0200

    small updated

--- src/components/AddCommentModal.js
+++ src/components/AddCommentModal.js
@@ -2,57 +2,22 @@ import React from "react";
 import { Button, Modal, Form, Badge } from "react-bootstrap";
 // import data from "../data";
 import SearchBar from "../components/SearchBar";
-
-const InputGroup = ({
-  name,
-  label,
-  type,
-  value,
-  placeholder,
-  description,
-  handleInput
-}) => {
-  return (
-    <Form.Group controlId="formBasicEmail">
-      <Form.Label>{label}</Form.Label>
-      <Form.Control
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={handleInput}
-        name={name}
-      />
-      <Form.Text className="text-muted">{description}</Form.Text>
-    </Form.Group>
-  );
-};
+import moodColorCode from "../moodColorCode";
+// import data from "../data";
 
 export default class Comments extends React.Component {
   state = {};
 
   render() {
     const { email, comment, mood, title } = this.props.state;
+
     const { data } = this.props;
-    const emailInput = {
-      name: "email",
-      label: "Email Address",
-      type: "email",
-      value: email,
-      placeholder: "Enter email",
-      description:
-        "This field is totally optional. If you want to keep your anonymity please do not fill it.",
-      handleInput: this.props.handleInput
-    };
-    const hashtagsInput = {
-      name: "hastags",
-      label: "Add Hashtags",
-      type: "text",
-      value: this.props.state.hashtags,
-      placeholder: "meeting.. mondays..",
-      description:
-        "Add some context to your feedback! Just got out of a skype meeting? Awesome colleague brought breakfast?",
-      handleInput: this.props.handleInput
-    };
+    // console.log("EEEEOO", this.props.state.currentMood);
+    const moodColorCodeBackground =
+      data[this.props.state.currentMood] === undefined
+        ? ""
+        : moodColorCode[data[this.props.state.currentMood].name];
+
     return (
       <>
         <Modal
@@ -71,7 +36,13 @@ export default class Comments extends React.Component {
           <Modal.Body>
             <Form.Label style={{ marginBottom: 10 }}>
               Today you are feeling{" "}
-              <Badge variant="secondary" style={{ fontSize: "105%" }}>
+              <Badge
+                variant="secondary"
+                style={{
+                  fontSize: "105%",
+                  backgroundColor: moodColorCodeBackground
+                }}
+              >
                 {data[this.props.state.currentMood] === undefined
                   ? ""
                   : data[this.props.state.currentMood].name}
@@ -83,18 +54,6 @@ export default class Comments extends React.Component {
                 this.props.handleSubmit(e);
               }}
             >
-              {/* {InputGroup(emailInput)} */}
-              {/* {InputGroup(hashtagsInput)} */}
-              {/* <Form.Group>
-                <Form.Label>Add a title</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="My awesome day!!"
-                  value={title}
-                  onChange={this.props.handleInput}
-                  name="title"
-                />
-              </Form.Group> */}
               <Form.Group>
                 <Form.Label>Add Hashtags</Form.Label>
                 <SearchBar
@@ -102,10 +61,6 @@ export default class Comments extends React.Component {
                   hashtags={this.props.hashtags}
                   handleHashtags={this.props.handleHashtags}
                 />
-                {/* <Form.Text>
-                  Add some context to your feedback! Just got out of a skype
-                  meeting? Awesome colleague brought breakfast? Tag it!
-                </Form.Text> */}
               </Form.Group>
               <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Give us the details</Form.Label>
